df37fa61fde044940070348ff6bc583e4a2b2960
hungdaycb00 <npmhungcb94@gmail.com>
2026-05-05 09:56:04 +0700

    Auto-deploy: Tue 05/05/2026  9:56:04.76

--- src/components/AlinMap.tsx
+++ src/components/AlinMap.tsx
@@ -317,6 +317,7 @@ const AlinMapInner: React.FC<AlinMapProps> = ({
                 setIsWidgetExpanded={setIsWeatherWidgetExpanded}
                 isSheetExpanded={nav.isSheetExpanded}
                 isDesktop={nav.isDesktop}
+                mainTab={nav.mainTab}
             />
 
             <NavigationBar mainTab={nav.mainTab} selectedUser={nav.selectedUser} isDesktop={nav.isDesktop} handleTabClick={nav.handleTabClick} user={user} isSheetExpanded={nav.isSheetExpanded} />
